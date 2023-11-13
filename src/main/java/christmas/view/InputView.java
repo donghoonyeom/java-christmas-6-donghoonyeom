0ea2d2198a0075dd95b73369a0f1b21d63bb5deb
donghoonyeom <dua5410773@naver.com>
2023-11-13 16:50:26 +0900

    feat(view): 주문 정보 출력 기능 추가
    이 커밋은 `OutputView` 클래스에 주문 정보를 출력하는 새로운 기능을 추가했습니다.
    
    - `printBadge` 메서드는 12월 이벤트 배지를 출력합니다.
    - `printPayment` 메서드는 할인 후 예상 결제 금액을 출력합니다.
    - `calculateGiftMenu` 메서드는 주문 금액에 따라 증정 메뉴를 계산합니다.
    - `printGiftMenu` 메서드는 증정 메뉴를 출력합니다.
    - `printBenefits` 메서드는 총 혜택 금액을 출력합니다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
 
     public static int getDate() {
         while (true) {
-            System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+            OutputView.printMessage("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
             String customerInput = getCustomerInput("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
             try {
                 int date = Integer.parseInt(customerInput);
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -32,9 +32,20 @@ public class OutputView {
     public static void printPreviewPhrase(int date) {
         System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n", date);
     }
+    public static String calculateGiftMenu(int totalOrderAmount) {
+        if (totalOrderAmount >= 120000) {
+            return GIFT_MENU;
+        }
+        return "없음";
+    }
 
     public static void printTotalOrderAmount(int totalOrderAmount) {
         printMessage(String.format("<할인 전 총주문 금액>%n%s원", formatAmount(totalOrderAmount)));
+        printEmptyLine();
+    }
+
+    public static void printGiftMenu(String giftMenu) {
+        printMessage(String.format("<증정 메뉴>%n%s", giftMenu));
         printEmptyLine();
     }
 
@@ -67,6 +78,20 @@ public class OutputView {
         }
     }
 
+
+    public static void printBenefits(int totalBenefits) {
+        printMessage(String.format("<총혜택 금액>%n-%s원", formatAmount(totalBenefits)));
+        printEmptyLine();
+    }
+
+    public static void printPayment(int totalPayment) {
+        printMessage(String.format("<할인 후 예상 결제 금액>%n%s원", formatAmount(totalPayment)));
+        printEmptyLine();
+    }
+
+    public static void printBadge(String badge) {
+        printMessage(String.format("<12월 이벤트 배지>%n%s", badge));
+    }
 
     private static String formatAmount(int amount) {
         return String.format("%,d", amount);
