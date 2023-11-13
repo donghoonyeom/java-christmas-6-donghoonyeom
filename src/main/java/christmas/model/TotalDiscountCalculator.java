206f174916ec5cf8ebf95a2ba95306f46058f03b
donghoonyeom <dua5410773@naver.com>
2023-11-13 16:39:44 +0900

    feat(model): TotalDiscountCalculator 클래스에 새로운 할인 계산 기능 추가
    이 커밋은 `TotalDiscountCalculator` 클래스에 새로운 할인 계산 기능을 추가했습니다.
    
    - `calculateDateDiscount` 메서드는 날짜에 따른 할인을 계산합니다. 이는 크리스마스 디데이 할인, 평일 할인, 주말 할인을 고려하여 총 할인을 반환합니다.
    - `calculateStarDiscount` 메서드는 특별 할인을 계산합니다. 별표가 붙은 특정 날짜에 할인이 적용됩니다.

--- src/main/java/christmas/model/TotalDiscountCalculator.java
+++ src/main/java/christmas/model/TotalDiscountCalculator.java
@@ -1,8 +1,21 @@
 package christmas.model;
 
+import java.util.List;
 
 public class TotalDiscountCalculator {
 
+    public static int calculateDateDiscount(int date, List<String> menus) {
+        int dateDiscount = DateDiscount.dayDiscount(date);
+        int weekDayDiscount = DateDiscount.weekDayDiscount(date, menus);
+        int weekendDayDiscount = DateDiscount.weekendDayDiscount(date, menus);
+
+        return dateDiscount + weekDayDiscount + weekendDayDiscount;
+    }
+
+
+    public static int calculateStarDiscount(int date) {
+        return StarDiscount.starDayDiscount(date);
+    }
 
     public static int calculateGiftMenu(int totalOrderAmount, int totalDiscount) {
         int totalBenefits = totalDiscount;
