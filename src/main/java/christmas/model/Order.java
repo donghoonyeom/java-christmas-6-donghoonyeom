ae4a5f3326f1581152d2f08b66ff0a45054eb297
donghoonyeom <dua5410773@naver.com>
2023-11-13 16:01:55 +0900

    feat(model): 주문(Order) 클래스 추가
    이 커밋은 사용자의 주문 정보를 나타내는 `Order` 클래스를 추가합니다. `Order` 클래스는 주문된 메뉴들을 관리하고, 주문 총액을 계산하는 기능을 제공합니다.
    
    - `createOrder` 메서드는 사용자로부터 날짜와 주문 정보를 입력받아 `Order` 객체를 생성합니다. 입력받은 주문 정보는 `,`를 기준으로 나누어 `List<String>` 형태로 저장됩니다. 또한, 출력뷰를 활용하여 날짜에 대한 미리보기 문구를 출력합니다.
    
    - `calculateTotalOrderAmount` 메서드는 주문된 메뉴들의 총 주문 금액을 계산합니다. 각 메뉴의 수량과 가격을 곱한 값을 더하여 총 주문 금액을 얻습니다.
    
    - `calculateMenuAmount` 메서드는 개별 메뉴의 주문 금액을 계산합니다. 메뉴 이름과 수량을 이용하여 해당 메뉴의 가격을 구하고, 수량과 곱하여 개별 메뉴의 주문 금액을 얻습니다.

---
+++ src/main/java/christmas/model/Order.java
@@ -0,0 +1,43 @@
+package christmas.model;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.Arrays;
+import java.util.List;
+
+public class Order {
+    private final List<String> menus;
+
+    public Order(List<String> menus) {
+        this.menus = menus;
+    }
+
+    public static Order createOrder(int date) {
+        String orderInput = InputView.getOrder();
+        List<String> menus = Arrays.asList(orderInput.split(","));
+        OutputView.printPreviewPhrase(date);
+        OutputView.printEmptyLine();
+        return new Order(menus);
+    }
+
+    public List<String> getMenus() {
+        return menus;
+    }
+
+    public int calculateTotalOrderAmount() {
+        int totalOrderAmount = 0;
+        for (String menu : menus) {
+            totalOrderAmount += calculateMenuAmount(menu.trim());
+        }
+        return totalOrderAmount;
+    }
+
+    private int calculateMenuAmount(String menu) {
+        String[] menuInfo = menu.split("-");
+
+        String menuName = menuInfo[0];
+        int quantity = Integer.parseInt(menuInfo[1]);
+
+        return quantity * Menu.valueOf(menuName).getPrice();
+    }
+}
