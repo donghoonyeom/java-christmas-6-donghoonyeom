496f2a56e91653186f2126df16f574864a4dcc7c
donghoonyeom <dua5410773@naver.com>
2023-11-13 16:20:26 +0900

    feat(model): DateDiscount 클래스 주말,평일 할인 기능 추가
    이 커밋은 `DateDiscount` 클래스를 업데이트했습니다. 새로운 기능을 추가하여 주말 및 평일에 따른 할인을 계산할 수 있도록 했습니다.
    
    - `weekendDayDiscount` 메서드는 입력된 날짜가 주말인 경우, 특정 주말 할인 규칙에 따라 메인 메뉴 주문에 대한 할인을 계산합니다.
    - `weekDayDiscount` 메서드는 입력된 날짜가 평일인 경우, 특정 주말 할인 규칙에 따라 디저트 메뉴 주문에 대한 할인을 계산합니다.

--- src/main/java/christmas/model/DateDiscount.java
+++ src/main/java/christmas/model/DateDiscount.java
@@ -1,12 +1,14 @@
 package christmas.model;
 
+import java.util.Arrays;
+import java.util.List;
 
 public class DateDiscount {
     private static final int DISCOUNT_START_AMOUNT = 1000;
     private static final int DISCOUNT_INCREASE_AMOUNT = 100;
     private static final int DISCOUNT_START_DAY = 1;
     private static final int DISCOUNT_END_DAY = 25;
-
+    private static final List<Integer> WEEKEND_DAYS = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
 
 
     public static int dayDiscount(int date) {
@@ -18,5 +20,43 @@ public class DateDiscount {
         return totalDiscount;
     }
 
-}
+    public static int weekendDayDiscount(int date, List<String> menus) {
+        int totalDiscount = 0;
+        if (WEEKEND_DAYS.contains(date)) {
+            for (String menu : menus) {
+                String[] menuInfo = menu.split("-");
+                String menuName = menuInfo[0];
+                int quantity = Integer.parseInt(menuInfo[1]);
 
+                if (isMainMenu(menuName)) {
+                    totalDiscount += quantity * 2023;
+                }
+            }
+        }
+        return totalDiscount;
+    }
+
+    public static int weekDayDiscount(int date, List<String> menus) {
+        int totalDiscount = 0;
+        if (!WEEKEND_DAYS.contains(date)) {
+            for (String menu : menus) {
+                String[] menuInfo = menu.split("-");
+                String menuName = menuInfo[0];
+                int quantity = Integer.parseInt(menuInfo[1]);
+
+                if (isDessertMenu(menuName)) {
+                    totalDiscount += quantity * 2023;
+                }
+            }
+        }
+        return totalDiscount;
+    }
+
+    private static boolean isMainMenu(String menuName) {
+        return Arrays.asList("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타").contains(menuName);
+    }
+
+    private static boolean isDessertMenu(String menuName) {
+        return Arrays.asList("초코케이크", "아이스크림").contains(menuName);
+    }
+}
