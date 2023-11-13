016fddf4cd3809e6e089048545069dcd99fbc8e4
donghoonyeom <dua5410773@naver.com>
2023-11-13 15:52:46 +0900

    feat(view): 주문 입력 기능 추가
    이 커밋은 사용자에게 주문을 입력받는 기능인 `getOrder` 메서드를 추가합니다. 사용자는 메뉴와 개수를 지정하여 주문을 할 수 있으며, 예시로 주어진 형식은 "메뉴이름-개수"입니다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,6 +1,8 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.model.Menu;
+import java.util.Arrays;
 
 public class InputView {
 
@@ -21,10 +23,41 @@ public class InputView {
                     throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             }
         }
+    }
+
+    public static String getOrder() {
+        while (true) {
+            String order = getCustomerInput("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+            try {
+                validateOrderInput(order);
+                return order;
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+        }
+    }
+
+    private static void validateOrderInput(String order) {
+        if (!order.matches("^[가-힣a-zA-Z0-9\\-]*(,[가-힣a-zA-Z0-9\\-]+-[0-9]+)*$")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        if (containsEnglish(order)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        String[] menuInfo = order.split("-");
+        String menuName = menuInfo[0];
+
+        if (Arrays.stream(Menu.values()).noneMatch(menu -> menu.name().equals(menuName))) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean containsEnglish(String order) {
+        return order.matches(".*[a-zA-Z]+.*");
     }
 }
