4363b63f9865e235dab288004345a9d7c928436b
donghoonyeom <dua5410773@naver.com>
2023-11-14 17:22:39 +0900

    refactor(view): 유효성 검사 향상을 위한 InputView 리팩토링
    이 커밋은 InputView 클래스를 리팩토링하여 유효성 검사를 강화하고 사용자 상호 작용을 개선했습니다. 변경 사항은 다음과 같습니다:
    
    1. **인사말 메시지 통합:** 인사말 메시지는 일관성을 유지하고 유지 관리를 쉽게 하기 위해 이제 상수로 정의되었습니다.
    
    2. **유효성 검사 및 검색 분리:** 날짜 및 주문의 유효성 검사 및 검색이 별도의 메서드 (`validateAndRetrieveOrder`, `validateAndRetrieveDate`)로 분리되었습니다. 이는 가독성을 향상시키고 깔끔한 구조를 유지하도록 도와줍니다.
    
    3. **유효성 검사 오류 처리:** 새로운 메서드 `handleValidationError`가 도입되어 유효성 검사 오류를 처리합니다. 이 메서드는 콘솔에 오류 메시지를 출력하여 사용자에게 명확한 피드백을 제공합니다.
    
    4. **예외 메시지:** 예외 메시지는 이제 일관되며 사용자에게 유효하지 않은 입력의 경우 안내하는 정보를 제공합니다.
    
    이러한 변경 사항은 InputView 클래스를 유지 관리 가능하고 가독성 있게 만들며 사용자 친화성을 향상시키기 위한 것입니다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,63 +1,58 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.model.Menu;
-import java.util.Arrays;
+import christmas.validator.DateValidator;
+import christmas.validator.OrderValidator;
 
 public class InputView {
 
-    private static String getCustomerInput(String message) {
-        System.out.println(message);
-        return Console.readLine();
-    }
+    private static final String GREETING_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
 
     public static int getDate() {
-        while (true) {
-            OutputView.printMessage("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
-            String customerInput = getCustomerInput("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-            try {
-                int date = Integer.parseInt(customerInput);
-                if (date >= 1 && date <= 31) {
-                    return date;
-                } else {
-                    throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-                }
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-            }
-        }
+        OutputView.printMessage(GREETING_MESSAGE);
+        return getValidatedDate();
     }
 
     public static String getOrder() {
+        return getValidatedOrder();
+    }
+
+    private static String getValidatedOrder() {
         while (true) {
-            String order = getCustomerInput("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
             try {
-                validateOrderInput(order);
-                return order;
+                return validateAndRetrieveOrder();
             } catch (IllegalArgumentException e) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                handleValidationError(e);
             }
         }
     }
 
-    private static void validateOrderInput(String order) {
-        if (!order.matches("^[가-힣a-zA-Z0-9\\-]*(,[가-힣a-zA-Z0-9\\-]+-[0-9]+)*$")) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
-
-        if (containsEnglish(order)) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
-
-        String[] menuInfo = order.split("-");
-        String menuName = menuInfo[0];
-
-        if (Arrays.stream(Menu.values()).noneMatch(menu -> menu.name().equals(menuName))) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    private static int getValidatedDate() {
+        while (true) {
+            try {
+                return validateAndRetrieveDate();
+            } catch (IllegalArgumentException e) {
+                handleValidationError(e);
+            }
         }
     }
 
-    private static boolean containsEnglish(String order) {
-        return order.matches(".*[a-zA-Z]+.*");
+    private static String validateAndRetrieveOrder() {
+        String order = getCustomerInput("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return OrderValidator.validateAndGetOrder(order);
+    }
+
+    private static int validateAndRetrieveDate() {
+        String customerInput = getCustomerInput("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return DateValidator.validateAndGetDate(customerInput);
+    }
+
+    private static String getCustomerInput(String message) {
+        OutputView.printMessage(message);
+        return Console.readLine();
+    }
+
+    private static void handleValidationError(IllegalArgumentException e) {
+        OutputView.printMessage(e.getMessage());
     }
 }
