d7d7166823c25c38e0b2e65fb5dbdcad4090a8e9
donghoonyeom <dua5410773@naver.com>
2023-11-14 17:09:12 +0900

    refactor(model): Menu 열거형 변경
    이 커밋에서는 Menu 열거형의 구조를 개선하고 새로운 기능을 추가했습니다.
    
    주요 변경사항은 다음과 같습니다:
    
    1. 메뉴 가격 메서드 추가: 샴페인의 가격을 반환하는 `getChampagnePrice` 메서드를 추가했습니다.
    
    2. 메뉴 이름으로 가격 조회 메서드 추가: 메뉴 이름을 전달받아 해당 메뉴의 가격을 반환하는 `getPriceByMenuName` 메서드를 도입했습니다.
    
    3. 음료, 메인, 디저트 메뉴 확인 메서드 추가: 각각의 카테고리에 속하는지 여부를 확인하는 `containsBeverageMenu`, `containsMainMenu`, `containsDessertMenu` 메서드를 추가했습니다.
    
    이 변경으로 인해 Menu 열거형은 더 유연해지고, 메뉴 관련 기능이 한 곳에 집중되어 가독성과 확장성이 향상되었습니다.

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -1,5 +1,7 @@
 package christmas.model;
 
+import java.util.Arrays;
+
 public enum Menu {
     양송이수프(6_000),
     타파스(5_500),
@@ -20,7 +22,27 @@ public enum Menu {
         this.price = price;
     }
 
+    public static int getChampagnePrice() {
+        return 샴페인.getPrice();
+    }
+
+    public static int getPriceByMenuName(String menuName) {
+        return Menu.valueOf(menuName).getPrice();
+    }
+
     public int getPrice() {
         return price;
+    }
+
+    public boolean containsBeverageMenu(String menuName) {
+        return Arrays.asList(제로콜라.name(), 레드와인.name(), 샴페인.name()).contains(menuName);
+    }
+
+    public boolean containsMainMenu(String menuName) {
+        return Arrays.asList(티본스테이크.name(), 바비큐립.name(), 해산물파스타.name(), 크리스마스파스타.name()).contains(menuName);
+    }
+
+    public boolean containsDessertMenu(String menuName) {
+        return Arrays.asList(초코케이크.name(), 아이스크림.name()).contains(menuName);
     }
 }
