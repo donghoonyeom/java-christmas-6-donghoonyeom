991ea2279b16625b53b022aca6c774adaefc62a3
donghoonyeom <dua5410773@naver.com>
2023-11-14 17:05:12 +0900

    refactor(model): DateDiscount를 명확성과 캡슐화를 향상하기 위한 수정
    이 커밋에서는 DateDiscount 클래스를 개선하기 위해 코드 가독성과 캡슐화를 강화했습니다. 주요 변경사항은 다음과 같습니다:
    
    - 메서드 이름 변경: `dayDiscount` 메서드의 이름을 목적을 더 잘 반영하는 `ddayDiscount`로 변경했습니다.
    
    - 캡슐화: 주어진 날짜가 할인 날짜인지를 확인하고 할인 금액을 계산하는 로직을 캡슐화하기 위해 새로운 private 메서드 (`isDiscountDay` 및 `calculateDayDiscount`)를 만들었습니다. 이로써 코드의 가독성과 유지보수성이 향상되었습니다.
    
    - 상수 리팩터링: 할인 날짜가 아닐 때의 기본 할인을 나타내기 위해 `DEFAULT_DISCOUNT`라는 새로운 상수를 도입했습니다. 이는 기본값에 의미 있는 이름을 제공함으로써 코드의 가독성을 향상시킵니다.

--- src/main/java/christmas/model/DateDiscount.java
+++ src/main/java/christmas/model/DateDiscount.java
@@ -1,62 +1,24 @@
 package christmas.model;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class DateDiscount {
     private static final int DISCOUNT_START_AMOUNT = 1000;
     private static final int DISCOUNT_INCREASE_AMOUNT = 100;
     private static final int DISCOUNT_START_DAY = 1;
     private static final int DISCOUNT_END_DAY = 25;
-    private static final List<Integer> WEEKEND_DAYS = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+    private static final int DEFAULT_DISCOUNT = 0;
 
-
-    public static int dayDiscount(int date) {
-        int totalDiscount = 0;
-        if (DISCOUNT_START_DAY <= date && date <= DISCOUNT_END_DAY) {
-            totalDiscount = DISCOUNT_START_AMOUNT + (date - DISCOUNT_START_DAY) * DISCOUNT_INCREASE_AMOUNT;
-            return totalDiscount;
+    public static int ddayDiscount(int date) {
+        if (isDiscountDay(date)) {
+            return calculateDayDiscount(date);
         }
-        return totalDiscount;
+        return DEFAULT_DISCOUNT;
     }
 
-    public static int weekendDayDiscount(int date, List<String> menus) {
-        int totalDiscount = 0;
-        if (WEEKEND_DAYS.contains(date)) {
-            for (String menu : menus) {
-                String[] menuInfo = menu.split("-");
-                String menuName = menuInfo[0];
-                int quantity = Integer.parseInt(menuInfo[1]);
-
-                if (isMainMenu(menuName)) {
-                    totalDiscount += quantity * 2023;
-                }
-            }
-        }
-        return totalDiscount;
+    private static boolean isDiscountDay(int date) {
+        return DISCOUNT_START_DAY <= date && date <= DISCOUNT_END_DAY;
     }
 
-    public static int weekDayDiscount(int date, List<String> menus) {
-        int totalDiscount = 0;
-        if (!WEEKEND_DAYS.contains(date)) {
-            for (String menu : menus) {
-                String[] menuInfo = menu.split("-");
-                String menuName = menuInfo[0];
-                int quantity = Integer.parseInt(menuInfo[1]);
-
-                if (isDessertMenu(menuName)) {
-                    totalDiscount += quantity * 2023;
-                }
-            }
-        }
-        return totalDiscount;
-    }
-
-    private static boolean isMainMenu(String menuName) {
-        return Arrays.asList("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타").contains(menuName);
-    }
-
-    private static boolean isDessertMenu(String menuName) {
-        return Arrays.asList("초코케이크", "아이스크림").contains(menuName);
+    private static int calculateDayDiscount(int date) {
+        return DISCOUNT_START_AMOUNT + (date - DISCOUNT_START_DAY) * DISCOUNT_INCREASE_AMOUNT;
     }
 }
