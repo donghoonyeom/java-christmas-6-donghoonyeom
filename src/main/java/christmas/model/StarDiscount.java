a5e2870379651d2ddfafe0d9ff327ad9f2fcc950
donghoonyeom <dua5410773@naver.com>
2023-11-14 17:14:09 +0900

    refactor(model): StarDiscount 클래스 리팩토링
    이 커밋에서는 StarDiscount 클래스를 리팩토링하여 가독성을 향상시켰습니다.
    
    변경된 내용은 다음과 같습니다:
    
    1. **기본 할인 값 도입:** STAR_DISCOUNT 상수와 함께 DEFAULT_DISCOUNT 상수를 도입하여, 할인이 적용되지 않은 경우 기본값을 명시적으로 표현했습니다.
    
    2. **조건문 단순화:** 조건문을 단순화하여 가독성을 높였습니다. 할인이 적용되는 경우 STAR_DISCOUNT를 반환하고, 그렇지 않은 경우 DEFAULT_DISCOUNT를 반환하도록 변경했습니다.
    
    이 변경으로 인해 StarDiscount 클래스는 더 명확하게 할인 로직을 표현하며, 코드의 일관성이 향상되었습니다.

--- src/main/java/christmas/model/StarDiscount.java
+++ src/main/java/christmas/model/StarDiscount.java
@@ -4,16 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StarDiscount {
-
     private static final List<Integer> STAR_DAYS = Arrays.asList(3, 10, 17, 18, 24, 25, 31);
-
     private static final int STAR_DISCOUNT = 1000;
+    private static final int DEFAULT_DISCOUNT = 0;
 
     public static int starDayDiscount(int date) {
-        int totalDiscount = 0;
         if (STAR_DAYS.contains(date)) {
-            totalDiscount += STAR_DISCOUNT;
+            return STAR_DISCOUNT;
         }
-        return totalDiscount;
+        return DEFAULT_DISCOUNT;
     }
 }
