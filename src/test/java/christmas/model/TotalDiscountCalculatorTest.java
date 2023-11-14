bdf4e8e9f248868c04a55338b1d7026e269e05ff
donghoonyeom <dua5410773@naver.com>
2023-11-15 03:48:44 +0900

    test: TotalDiscountCalculator 클래스의 다양한 할인 메서드 테스트 추가
    이 커밋은 TotalDiscountCalculator 클래스의 여러 할인 메서드를 테스트하는 코드를 추가했습니다.
    
    변경 사항:
    1. `calculateDateDiscount` 테스트: 날짜 할인이 올바르게 계산되는지 확인하는 테스트를 추가했습니다. 특정 날짜에 대한 할인 금액이 올바르게 반환되는지 확인합니다.
    2. `calculateStarDiscount` 테스트: 스타 할인이 올바르게 계산되는지 확인하는 테스트를 추가했습니다. 특정 날짜에 대한 스타 할인 금액이 올바르게 반환되는지 확인합니다.
    3. `calculateGiftMenu` 테스트: 기프트 메뉴가 올바르게 계산되는지 확인하는 테스트를 추가했습니다. 전체 주문 금액과 할인 금액을 기반으로 기프트 메뉴의 가격이 올바르게 반환되는지 확인합니다.
    4. `printGiftMenuPrice` 테스트: 선물 메뉴의 가격이 정상적으로 출력되는지 확인하는 테스트를 추가했습니다. 전체 주문 금액을 기반으로 선물 메뉴의 가격이 올바르게 반환되는지 확인합니다.

---
+++ src/test/java/christmas/model/TotalDiscountCalculatorTest.java
@@ -0,0 +1,65 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.HashSet;
+import java.util.Set;
+
+@DisplayName("TotalDiscountCalculator 테스트")
+class TotalDiscountCalculatorTest {
+
+    @Test
+    @DisplayName("날짜 할인이 올바르게 계산되는지 확인")
+    void calculateDateDiscount() {
+        // Given
+        int date = 3;
+        Set<OrderItem> menus = new HashSet<>();
+
+        // When
+        int result = TotalDiscountCalculator.calculateDateDiscount(date, menus);
+
+        // Then
+        assertThat(result).isEqualTo(1200);
+    }
+
+    @Test
+    @DisplayName("스타 할인이 올바르게 계산되는지 확인")
+    void calculateStarDiscount() {
+        // Given
+        int date = 10;
+
+        // When
+        int result = TotalDiscountCalculator.calculateStarDiscount(date);
+
+        // Then
+        assertThat(result).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("기프트 메뉴가 올바르게 계산되는지 확인")
+    void calculateGiftMenu() {
+        // Given
+        int totalOrderAmount = 130000;
+        int totalDiscount = 5000;
+
+        // When
+        int result = TotalDiscountCalculator.calculateGiftMenu(totalOrderAmount, totalDiscount);
+
+        // Then
+        assertThat(result).isEqualTo(totalDiscount + Menu.getChampagnePrice());
+    }
+
+    @Test
+    @DisplayName("선물 메뉴의 가격이 정상적으로 출력되는지 확인")
+    void printGiftMenuPrice() {
+        // Given
+        int totalOrderAmount = 130000;
+
+        // When
+        int result = TotalDiscountCalculator.printGiftMenuPrice(totalOrderAmount);
+
+        // Then
+        assertThat(result).isEqualTo(Menu.getChampagnePrice());
+    }
+}
