3183ba37ea60d1f1560644e77cce12c5501f84a3
donghoonyeom <dua5410773@naver.com>
2023-11-15 19:02:24 +0900

    test: DateValidator 클래스의 테스트 코드 업데이트
    DateValidator 클래스의 테스트 코드가 변경되었습니다. 변경된 테스트 코드는 유효한 날짜와 유효하지 않은 날짜에 대한 입력에 대해 각각 ParameterizedTest를 사용하여 정상적으로 처리되는지 및 예외가 발생하는지를 확인하도록 수정되었습니다.

--- src/test/java/christmas/validator/DateValidatorTest.java
+++ src/test/java/christmas/validator/DateValidatorTest.java
@@ -4,40 +4,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("날짜 입력 예외 테스트")
 class DateValidatorTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"15", "1", "31"}) // 유효한 날짜들
     @DisplayName("유효한 날짜가 입력될 때 정상적으로 처리되는지 확인")
-    void validateAndGetDate_ValidDate() {
-        // Given
-        String input = "15";
-
+    void validateAndGetDate_ValidDate(String input) {
         // When
         int result = DateValidator.validateAndGetDate(input);
 
         // Then
-        assertThat(result).isEqualTo(15);
+        assertThat(result).isEqualTo(Integer.parseInt(input));
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "-1", "32", "100"})
     @DisplayName("유효하지 않은 숫자 형식의 날짜가 입력될 때 예외가 발생하는지 확인")
-    void validateAndGetDate_InvalidNumberFormat() {
-        // Given
-        String input = "abc";
-
-        // When, Then
-        assertThrows(IllegalArgumentException.class, () -> DateValidator.validateAndGetDate(input));
-    }
-
-    @Test
-    @DisplayName("유효하지 않은 날짜 범위가 입력될 때 예외가 발생하는지 확인")
-    void validateAndGetDate_InvalidDateRange() {
-        // Given
-        String input = "40";
-
+    void validateAndGetDate_InvalidNumberFormat(String input) {
         // When, Then
         assertThrows(IllegalArgumentException.class, () -> DateValidator.validateAndGetDate(input));
     }
