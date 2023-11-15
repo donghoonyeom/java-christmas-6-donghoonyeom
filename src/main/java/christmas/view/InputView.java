44a48d45d1414b1e1d064476b541f10b9f1432a2
donghoonyeom <dua5410773@naver.com>
2023-11-15 19:06:46 +0900

    refactor(view): InputView 클래스의 코드 리팩토링
    InputView 클래스의 코드가 개선되었습니다. 변경된 내용은 주문 입력 메시지와 날짜 입력 메시지를 상수로 정의하고, 각각의 입력 메서드에서 해당 상수를 사용하여 메시지를 출력하도록 변경되었습니다. 이로써 코드의 가독성과 유지보수성이 향상되었습니다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -7,6 +7,8 @@ import christmas.validator.OrderValidator;
 public class InputView {
 
     private static final String GREETING_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String ORDER_INPUT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String DATE_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
     public static int getDate() {
         OutputView.printMessage(GREETING_MESSAGE);
@@ -38,13 +40,13 @@ public class InputView {
     }
 
     private static String validateAndRetrieveOrder() {
-        String order = getCustomerInput("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        return OrderValidator.validateAndGetOrder(order);
+        String orderInput = getCustomerInput(ORDER_INPUT_MESSAGE);
+        return OrderValidator.validateAndGetOrder(orderInput);
     }
 
     private static int validateAndRetrieveDate() {
-        String customerInput = getCustomerInput("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        return DateValidator.validateAndGetDate(customerInput);
+        String dateInput = getCustomerInput(DATE_INPUT_MESSAGE);
+        return DateValidator.validateAndGetDate(dateInput);
     }
 
     private static String getCustomerInput(String message) {
