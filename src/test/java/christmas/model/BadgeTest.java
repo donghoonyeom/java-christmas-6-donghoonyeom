a8fa16296b16467ff1813ca73e1b3345309c4972
donghoonyeom <dua5410773@naver.com>
2023-11-15 18:54:07 +0900

    test: Badge 클래스의 테스트 코드 업데이트
    Badge 클래스의 테스트 코드가 변경되었습니다. 주요 변경 사항은 혜택 범주에 따라 배지가 정확하게 반환되는지 확인하기 위한 매개 변수화된 테스트 케이스의 추가입니다. 이로써 각 배지 범주의 경계 조건을 더 효과적으로 검증할 수 있게 되었습니다.

--- src/test/java/christmas/model/BadgeTest.java
+++ src/test/java/christmas/model/BadgeTest.java
@@ -1,10 +1,12 @@
 import christmas.model.Badge;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("Badge 테스트")
+@DisplayName("배지 테스트")
 class BadgeTest {
 
     @Test
@@ -15,58 +17,34 @@ class BadgeTest {
         assertEquals("없음", badgeName);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {4000, 3000 ,2000, 1000})
     @DisplayName("혜택이 별 기준 미만인 경우 None 배지를 반환해야 함")
-    void calculateBadge_LessThanStarThreshold_ReturnsNoneBadge() {
-        int totalBenefits = 4000;
+    void calculateBadge_LessThanStarThreshold_ReturnsNoneBadge(int totalBenefits) {
         String badgeName = Badge.calculateBadge(totalBenefits);
         assertEquals("없음", badgeName);
     }
 
-    @Test
-    @DisplayName("혜택이 별 기준인 경우 별 배지를 반환해야 함")
-    void calculateBadge_StarThreshold_ReturnsStarBadge() {
-        int totalBenefits = 5000;
+    @ParameterizedTest
+    @ValueSource(ints = {5000, 8000, 9000})
+    @DisplayName("혜택이 별 기준 이상인 경우 별 배지를 반환해야 함")
+    void calculateBadge_StarThreshold_ReturnsStarBadge(int totalBenefits) {
         String badgeName = Badge.calculateBadge(totalBenefits);
         assertEquals("별", badgeName);
     }
 
-    @Test
-    @DisplayName("혜택이 별 기준과 트리 기준 사이인 경우 별 배지를 반환해야 함")
-    void calculateBadge_BetweenStarAndTreeThreshold_ReturnsStarBadge() {
-        int totalBenefits = 8000;
-        String badgeName = Badge.calculateBadge(totalBenefits);
-        assertEquals("별", badgeName);
-    }
-
-    @Test
-    @DisplayName("혜택이 트리 기준인 경우 트리 배지를 반환해야 함")
-    void calculateBadge_TreeThreshold_ReturnsTreeBadge() {
-        int totalBenefits = 10000;
+    @ParameterizedTest
+    @ValueSource(ints = {10000, 15000, 17000})
+    @DisplayName("혜택이 트리 기준 이상인 경우 트리 배지를 반환해야 함")
+    void calculateBadge_TreeThreshold_ReturnsTreeBadge(int totalBenefits) {
         String badgeName = Badge.calculateBadge(totalBenefits);
         assertEquals("트리", badgeName);
     }
 
-    @Test
-    @DisplayName("혜택이 트리 기준과 그 이상인 경우 트리 배지를 반환해야 함")
-    void calculateBadge_BetweenTreeAndSantaThreshold_ReturnsSantaBadge() {
-        int totalBenefits = 15000;
-        String badgeName = Badge.calculateBadge(totalBenefits);
-        assertEquals("트리", badgeName);
-    }
-
-    @Test
-    @DisplayName("혜택이 산타 기준인 경우 산타 배지를 반환해야 함")
-    void calculateBadge_SantaThreshold_ReturnsSantaBadge() {
-        int totalBenefits = 20000;
-        String badgeName = Badge.calculateBadge(totalBenefits);
-        assertEquals("산타", badgeName);
-    }
-
-    @Test
-    @DisplayName("혜택이 산타 기준을 초과한 경우 산타 배지를 반환해야 함")
-    void calculateBadge_AboveSantaThreshold_ReturnsSantaBadge() {
-        int totalBenefits = 25000;
+    @ParameterizedTest
+    @ValueSource(ints = {20000, 25000, 30000})
+    @DisplayName("혜택이 산타 기준 이상인 경우 산타 배지를 반환해야 함")
+    void calculateBadge_AboveSantaThreshold_ReturnsSantaBadge(int totalBenefits) {
         String badgeName = Badge.calculateBadge(totalBenefits);
         assertEquals("산타", badgeName);
     }
